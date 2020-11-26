cad592d4bc1530bd076415d6c6245c491d4c0314
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2020-11-26 00:20:13 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 from nupy.iterate import intermediate_value, secant
 
-iterations_2 = secant(function="x**3-5", variable="x", intervals=[1, 2], tolerance=1e-60, just_root=True)
-print(iterations_2)
+def roots():
+  iterations_2 = secant(function="x**3-5", variable="x", intervals=[1, 2], tolerance=1e-60, just_root=True)
+  iterations = intermediate_value(function="x**3-5", variable="x", intervals=[1, 2], tolerance=1e-60, just_root=True)
+  return [iterations, iterations_2]
 
-iterations_2 = secant(function="x**3-5", variable="x", intervals=[1, 2], tolerance=1e-60)
-print(iterations_2.head(15))
+# iterations_2 = secant(function="x**3-5", variable="x", intervals=[1, 2], tolerance=1e-60)
+# print(iterations_2.head(15))
 
-iterations = intermediate_value(function="x**3-5", variable="x", intervals=[1, 2], tolerance=1e-60, just_root=True)
-print(iterations)
+# iterations = intermediate_value(function="x**3-5", variable="x", intervals=[1, 2], tolerance=1e-60)
+# print(iterations.head(15))
 
-iterations = intermediate_value(function="x**3-5", variable="x", intervals=[1, 2], tolerance=1e-60)
-print(iterations.head(15))
+def test_answer():
+  for i in roots():
+    assert i == 1.709975946676697 
